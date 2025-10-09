2f1e8009d54e053ba5c3fc050bbe10a3596fbc18
File14 <filip.kjellsson@gmail.com>
2025-10-09 15:07:19 +0200

    Use BlockDisplay instead of ArmorStand for MoltenSnake

--- core/src/main/java/se/file14/procosmetics/cosmetic/mount/type/MoltenSnake.java
+++ core/src/main/java/se/file14/procosmetics/cosmetic/mount/type/MoltenSnake.java
@@ -2,12 +2,10 @@ package se.file14.procosmetics.cosmetic.mount.type;
 
 import org.bukkit.Location;
 import org.bukkit.Material;
-import org.bukkit.entity.Entity;
-import org.bukkit.entity.EntityType;
-import org.bukkit.entity.MagmaCube;
-import org.bukkit.entity.Player;
-import org.bukkit.inventory.ItemStack;
+import org.bukkit.block.data.BlockData;
+import org.bukkit.entity.*;
 import org.bukkit.util.Vector;
+import org.joml.Matrix4f;
 import se.file14.procosmetics.api.cosmetic.CosmeticContext;
 import se.file14.procosmetics.api.cosmetic.mount.MountBehavior;
 import se.file14.procosmetics.api.cosmetic.mount.MountType;
@@ -17,7 +15,7 @@ import se.file14.procosmetics.nms.EntityTrackerImpl;
 public class MoltenSnake implements MountBehavior {
 
     private static final int TAILS = 20;
-    private static final ItemStack ITEM_STACK = new ItemStack(Material.NETHERRACK);
+    private static final BlockData BLOCK_DATA = Material.NETHERRACK.createBlockData();
 
     private final EntityTrackerImpl tracker = new EntityTrackerImpl();
     private final Location reusableEntityLocation = new Location(null, 0.0d, 0.0d, 0.0d);
@@ -32,17 +30,23 @@ public class MoltenSnake implements MountBehavior {
             cube.setSize(2);
         }
         Location location = context.getPlayer().getLocation();
-        Vector vector = location.getDirection().multiply(-0.25d);
+        Vector vector = location.getDirection().multiply(-0.5d);
+
+        Matrix4f transformationMatrix = new Matrix4f();
+        transformationMatrix.identity()
+                .scale(0.5f)
+                //.rotateY(radians)
+                .translate(-0.5f, 0.5f, -0.5f);
 
         for (int i = 0; i < TAILS; i++) {
-            NMSEntity tailEntity = context.getPlugin().getNMSManager().createEntity(location.getWorld(), EntityType.ARMOR_STAND, tracker);
+            NMSEntity tailEntity = context.getPlugin().getNMSManager().createEntity(location.getWorld(), EntityType.BLOCK_DISPLAY, tracker);
+
+            if (tailEntity.getBukkitEntity() instanceof BlockDisplay blockDisplay) {
+                blockDisplay.setBlock(BLOCK_DATA);
+                blockDisplay.setTransformationMatrix(transformationMatrix);
+                blockDisplay.setTeleportDuration(1);
+            }
             tailEntity.setPositionRotation(location.add(vector));
-            tailEntity.setHelmet(ITEM_STACK);
-            tailEntity.setInvisible(true);
-            tailEntity.setArmorStandBasePlate(false);
-            tailEntity.setArmorStandMarker(true);
-            tailEntity.setArmorStandArms(false);
-            tailEntity.setArmorStandSmall(true);
         }
         tracker.startTracking();
     }
@@ -50,26 +54,20 @@ public class MoltenSnake implements MountBehavior {
     @Override
     public void onUpdate(CosmeticContext<MountType> context, Entity entity, NMSEntity nmsEntity) {
         entity.getLocation(reusableEntityLocation);
-        Location tempLocation = reusableEntityLocation;
-        float yaw = tempLocation.getYaw();
+        Location tempLocation;
         Player player = context.getPlayer();
 
         if (player.getVehicle() == entity) {
             tempLocation = player.getLocation();
-            yaw = tempLocation.getYaw();
-
-            nmsEntity.setYaw(yaw);
             entity.setVelocity(tempLocation.getDirection().multiply(0.7d));
         }
-        Location location = reusableEntityLocation.add(0.0d, -0.5d, 0.0d);
+        Location location = reusableEntityLocation;
 
         for (NMSEntity tailEntity : tracker.getEntities()) {
             Location temp = tailEntity.getPreviousLocation().clone();
 
             if (temp != reusableEntityLocation) {
                 tailEntity.sendPositionRotationPacket(location);
-                tailEntity.setHeadPose(tempLocation.getPitch(), yaw, 0.0f);
-                tailEntity.sendMetadataPacket();
                 location = temp;
             }
         }
