3a05aad821583017960bcd353037839132214e6e
File14 <file14@users.noreply.github.com>
2025-10-29 02:44:13 +0100

    Add license header to all classes

--- core/src/main/java/se/file14/procosmetics/util/ReflectionUtil.java
+++ core/src/main/java/se/file14/procosmetics/util/ReflectionUtil.java
@@ -1,3 +1,20 @@
+/*
+ * This file is part of ProCosmetics - https://github.com/File14/ProCosmetics
+ * Copyright (C) 2025 File14 and contributors
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
 package se.file14.procosmetics.util;
 
 import org.bukkit.Bukkit;
@@ -175,7 +192,7 @@ public class ReflectionUtil {
 
     private static boolean classList(Class<?>[] l1, Class<?>[] l2) {
         boolean equal = true;
-        
+
         if (l1.length != l2.length) {
             return false;
         }
